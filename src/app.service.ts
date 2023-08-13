552bc8a0773c2b597e5997a7c865cff3ff79f41a
Hasintha Doluweera <hasintha.doluweera@axiatadigitallabs.com>
2023-08-13 19:15:27 +0530

    save changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -32,7 +32,7 @@ import { MyLogger } from './logger/logger.service';
 export class AppService {
   constructor(private readonly logger: MyLogger) {}
 
-  @Cron(CronExpression.EVERY_MINUTE)
+  @Cron(CronExpression.EVERY_DAY_AT_7PM)
   async triggerPayments() {
     try {
       this.logger.log(
